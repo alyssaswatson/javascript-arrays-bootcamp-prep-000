6ac404a2bb6c7d098188bab32e6fc254a27f4b0a
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-11 14:40:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-addElementToBeginningOfArray(array, element) {
-  return `${element}, ${array}`
-}
+
 
 
 var destructivelyAddElementToBeginningOfArray =[];
